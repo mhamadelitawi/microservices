67fa7c64397b9be07ace70307f0fab6e28405ed1
mhamadelitawi <mhamadelitawi@gmail.com>
2021-10-01 01:15:53 +0300

    retry process

---
+++ kafka/kafka-admin/src/main/java/com/microservices/kafka/admin/client/KafkaAdminClient.java
@@ -0,0 +1,33 @@
+package com.microservices.kafka.admin.client;
+
+import com.microservices.config.KafkaConfigData;
+import com.microservices.config.RetryConfigData;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.retry.support.RetryTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaAdminClient {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaAdminClient.class);
+
+    private KafkaConfigData kafkaConfigData;
+
+    private RetryConfigData retryConfigData;
+
+    private AdminClient adminClient;
+
+    private RetryTemplate retryTemplate;
+
+    public KafkaAdminClient(KafkaConfigData config,
+                            RetryConfigData retryConfigData,
+                            AdminClient client,
+                            RetryTemplate template) {
+        this.kafkaConfigData = config;
+        this.retryConfigData = retryConfigData;
+        this.adminClient = client;
+        this.retryTemplate = template;
+    }
+}
